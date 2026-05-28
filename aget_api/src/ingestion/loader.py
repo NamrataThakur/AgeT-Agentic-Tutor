a99cc24f845ff8ed490eb00b1f4ed3993ab37ecf
NamrataThakur <namrata.thakur893@gmail.com>
2026-05-28 20:28:06 +0530

    Updated loader code committed

--- aget_api/src/ingestion/loader.py
+++ aget_api/src/ingestion/loader.py
@@ -7,120 +7,135 @@ from tqdm import tqdm
 from langchain_community.document_loaders import WebBaseLoader, WikipediaLoader, AsyncHtmlLoader
 from langchain_core.documents import Document
 
-from models import TopicsExtract, Topics, TopicsFactory
+import os
+import sys
+os.pardir
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+
+from pydantic_models.models import TopicsExtract, Topics, TopicsFactory
 from parser import *
 
 import nest_asyncio
 nest_asyncio.apply()
 
-def get_extractor(topics: list[TopicsExtract]) -> Generator[list[Topics, list[Document]], None, None]:
-    """Extract documents for a list of topics, yielding one at a time.
 
-    Args:
-        topics: A list of Topics objects containing topic information.
-
-    Yields:
-        List[Topics, list[Document]]: A list containing the topic object and a list of
-            documents extracted for that topic.
-
-    Note: "Topic" here means concepts on which AgeT can interview the user. Example: Logistic Regression, Clustering etc
-    """
-
-    progress_bar = tqdm(
-        topics,
-        desc="Extracting docs",
-        unit="topic",
-        bar_format="{desc}: {percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}] {postfix}",
-        ncols=100,
-        position=0,
-        leave=True,
-    )
-
-    topic_factory = TopicsFactory()
-    for topic in progress_bar:
-        topic_obj = topic_factory.get_topic(topic.id)
-
-        progress_bar.set_postfix_str(f"Topic: {topic_obj.name}")
-
-        topic_docs = extract_docs(topic_obj, topic.urls)
-
-        return [topic_obj, topic_docs]
-    
+class InformationExtractor:
+    def __init__(self):
+        self.topic_factory = TopicsFactory()
 
 
-def extract_docs(topic : Topics, extract_urls : list[str]) -> list[Document]:
-    """Extract documents for a single topic from all sources and deduplicate them.
+    def get_extractor(self, topics: list[TopicsExtract]) -> Generator[list[Topics, list[Document]], None, None]:
+        """Extract documents for a list of topics, yielding one at a time.
 
-    Args:
-        topic: Topics object containing topic information.
-        extract_urls: List of URLs to extract content from.
+        Args:
+            topics: A list of Topics objects containing topic information.
 
-    Returns:
-        list[Document]: List of documents extracted for the topic.
-    """
+        Yields:
+            List[Topics, list[Document]]: A list containing the topic object and a list of
+                documents extracted for that topic.
 
-    print("Information Extraction Stage Started ...!")
+        Note: "Topic" here means concepts on which AgeT can interview the user. Example: Logistic Regression, Clustering etc
+        """
 
-    print(f"Fetching raw html from : {extract_urls}")
-    loader = WebBaseLoader(extract_urls,
-                             header_template={
-                                 "User-Agent": (
-                                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                                "AppleWebKit/537.36 (KHTML, like Gecko) "
-                                "Chrome/124.0.0.0 Safari/537.36"
-                                )
-                            })
-    docs = loader.load()
+        progress_bar = tqdm(
+            topics,
+            desc="Extracting docs",
+            unit="topic",
+            bar_format="{desc}: {percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}] {postfix}",
+            ncols=100,
+            position=0,
+            leave=True,
+        )
 
-    transformed_docs = []
+        for topic in progress_bar:
+            topic_obj = self.topic_factory.get_topic(topic.id)
 
-    for doc in docs:
-        soup = BeautifulSoup(doc.page_content, "lxml")
+            progress_bar.set_postfix_str(f"Topic: {topic_obj.name}")
+
+            topic_docs = self.extract_docs(topic_obj, topic.urls)
+
+            return [topic_obj, topic_docs]
         
-        for tag in soup(["script", "style", "sup"]):
-            tag.decompose()
-        
-        for cls in ["toc", "navbox", "reference", "reflist", "mw-editsection", "sidebar", "metadata"]:
-            for tag in soup.select(f".{cls}"):
+
+
+    def extract_docs(self, topic : Topics, extract_urls : list[str]) -> list[Document]:
+        """Extract documents for a single topic from all sources and deduplicate them.
+
+        Args:
+            topic: Topics object containing topic information.
+            extract_urls: List of URLs to extract content from.
+
+        Returns:
+            list[Document]: List of documents extracted for the topic.
+        """
+
+        print("Information Extraction Stage Started ...!")
+
+        print(f"Fetching raw html from : {extract_urls}")
+        loader = WebBaseLoader(extract_urls,
+                                header_template={
+                                    "User-Agent": (
+                                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                                    "AppleWebKit/537.36 (KHTML, like Gecko) "
+                                    "Chrome/124.0.0.0 Safari/537.36"
+                                    )
+                                })
+        docs = loader.load()
+
+        transformed_docs = []
+
+        for doc in docs:
+            soup = BeautifulSoup(doc.page_content, "lxml")
+            
+            for tag in soup(["script", "style", "sup"]):
                 tag.decompose()
+            
+            for cls in ["toc", "navbox", "reference", "reflist", "mw-editsection", "sidebar", "metadata"]:
+                for tag in soup.select(f".{cls}"):
+                    tag.decompose()
 
-        math_elements = soup.select('annotation[encoding="application/x-tex"]')
+            math_elements = soup.select('annotation[encoding="application/x-tex"]')
 
-        for elem in math_elements:
+            for elem in math_elements:
 
-                latex = elem.get_text(strip=True)
+                    latex = elem.get_text(strip=True)
 
-                math_tag = elem.find_parent("math")
+                    math_tag = elem.find_parent("math")
 
-                if math_tag:
-                    math_tag.replace_with(f"\n$$ {latex} $$\n")
+                    if math_tag:
+                        math_tag.replace_with(f"\n$$ {latex} $$\n")
 
-        content = (
-                        soup.select_one(".mw-parser-output")
-                        or soup.select_one(".mw-body-content")
-                        or soup.select_one("#mw-content-text")
-                        or soup.body
-                    )
+            content = (
+                            soup.select_one(".mw-parser-output")
+                            or soup.select_one(".mw-body-content")
+                            or soup.select_one("#mw-content-text")
+                            or soup.body
+                        )
 
-        if content is None:
-            continue
-        
-        clean_text = content.get_text(" ", strip=True)
-        clean_text = replace_displaystyle(clean_text)
-        clean_text = normalize_whitespace(clean_text)
+            if content is None:
+                continue
+            
+            clean_text = content.get_text(" ", strip=True)
+            clean_text = replace_displaystyle(clean_text)
+            clean_text = normalize_whitespace(clean_text)
 
-        doc.page_content = clean_text
-        doc.metadata['topic_id'] = topic.id
-        doc.metadata['topic_name'] = topic.name
-        transformed_docs.append(doc)
-        
-    print("Information Extraction Stage Completed ...!")
-    return transformed_docs
+            doc.page_content = clean_text
+            doc.metadata['topic_id'] = topic.id
+            doc.metadata['topic_name'] = topic.name
+            transformed_docs.append(doc)
+            
+        print("Information Extraction Stage Completed ...!")
+        return transformed_docs
 
 
 if __name__ == "__main__":
     log_reg = TopicsFactory().get_topic("logistic_regression")
-    docs = extract_docs(
+    info_extractor = InformationExtractor()
+
+    docs = info_extractor.extract_docs(
         log_reg,
         [
             "https://en.wikipedia.org/wiki/Logistic_regression"
